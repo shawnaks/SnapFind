c7e2d0329be88c45ee37549576ffae04af54e1d8
Le Viet Bach <vietbach0407@gmail.com>
2025-09-27 16:13:37 -0400

    navbar reword

--- client/LostFound/src/components/Navbar.tsx
+++ client/LostFound/src/components/Navbar.tsx
@@ -6,11 +6,11 @@ export default function Navbar() {
     <header className="navbar">
       <div className="navbar__logo-wrap">
         <img className="navbar__logo-img" src="/vite.svg" alt="Logo" />
-        <Link to="/home" className="navbar__logo-text">Lost&Found</Link>
+        <Link to="/home" className="navbar__logo-text">Lost & Found</Link>
       </div>
       <nav className="navbar__nav navbar__nav--right">
-        <NavLink to="/post-lost" className="navbar__nav-link">Post lost</NavLink>
-        <NavLink to="/post-found" className="navbar__nav-link">Post found</NavLink>
+        <NavLink to="/post-lost" className="navbar__nav-link">Post Lost Items</NavLink>
+        <NavLink to="/post-found" className="navbar__nav-link">Post Found Items</NavLink>
         <NavLink to="/profile" className="navbar__nav-link">Profile</NavLink>
         <button className="navbar__nav-logout" type="button">Log out</button>
       </nav>
